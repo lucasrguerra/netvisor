46f506a3f8898a85fc3b31d7fe6158186c959f43
lucasrguerra <l.rayanguerra@gmail.com>
2025-01-08 12:43:37 -0300

    Correções e melhorias

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from sources import speedtest
 from sources import database
 from sources import latency
 from sources import utils
+from sources import speed
 import time
 
 def main():
@@ -20,9 +20,11 @@ def main():
         for host in host_list:
             latency_average, jitter_average, failed = latency.makeLatencyTest(host, 10)
             db.insertLatencyTest(host, latency_average, jitter_average, failed)
+            print("Host: ", host)
 
-
-        download_speed, upload_speed = speedtest.makeSpeedTest()
+        download_speed, upload_speed = speed.makeSpeedTest()
+        print("Download Speed: ", download_speed)
+        print("Upload Speed: ", upload_speed)
         db.insertSpeedTest(download_speed, upload_speed)
         
         time.sleep(600)
